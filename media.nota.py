003330abcadd22d6c50ad59df4cd50c736a1b0cc
1503-ng <55881192+1503-ng@users.noreply.github.com>
2020-03-23 01:00:41 -0300

    Cálculo de aritmética
    Conversões de de tipos de variáveis, operações com valores inteiros e fracionários.

---
+++ media.nota.py
@@ -0,0 +1,19 @@
+primeira_nota = input("Digite a primeira nota:" )
+
+segunda_nota = input("Digite a segunda nota: ")
+
+terceira_nota = input("Digite a terceira nota: ")
+
+quarta_nota = input("Digite a quarta nota: ")
+
+
+nota1 = int(primeira_nota)
+
+nota2 = int(segunda_nota)
+
+nota3 = int(terceira_nota)
+
+nota4 = int(quarta_nota)
+
+media = (nota1 + nota2 + nota3 + nota4)/4
+print("A média aritmética é", media)
