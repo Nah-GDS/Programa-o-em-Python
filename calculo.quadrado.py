b76192c51ee133c1903261a60e8f572f06db61eb
1503-ng <55881192+1503-ng@users.noreply.github.com>
2020-03-23 00:58:30 -0300

    Realizando cálculos
    Testando conversões de tipos de variáveis, operações.

---
+++ calculo.quadrado.py
@@ -0,0 +1,14 @@
+quadrado_lado = input("Digite o valor do lado de um quadrado:")
+
+lado = int(quadrado_lado)
+
+perimetro =(lado+lado)
+
+area = (lado*lado)
+
+
+
+print("perimetro:",perimetro,"- área:",area)
+
+
+
