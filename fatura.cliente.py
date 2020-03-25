d773110a27d2590b43d378b0ceb7d5955ba53ecb
1503-ng <55881192+1503-ng@users.noreply.github.com>
2020-03-25 18:01:26 -0300

    Imprimindo valores de fatura de cliente

---
+++ fatura.cliente.py
@@ -0,0 +1,13 @@
+Nome_Cliente = input("Digite o nome do cliente:")
+
+Dia_Vencimento = input("Digite o dia de vencimento:")
+
+Mes_Vencimento = input("Digite o mês de vencimento:")
+
+Valor_Fatura = input("Digite o valor da fatura:")
+
+print("Olá, ", Nome_Cliente,
+
+      "\n A sua fatura com vencimento em ",
+
+      Dia_Vencimento, Mes_Vencimento,"no valor de R$",Valor_Fatura,"está fechada.")
