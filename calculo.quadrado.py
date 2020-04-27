5e518880162a9450efadb97d7d5db927655042d8
Nayara Gomes da Silva <55881192+1503-ng@users.noreply.github.com>
2020-04-27 09:42:38 -0300

    Update calculo.quadrado.py

--- calculo.quadrado.py
+++ calculo.quadrado.py
@@ -1,3 +1,5 @@
+#Era necessário neste exercício proposto criar um input para receber o valor através do usuário.
+
 quadrado_lado = input("Digite o valor do lado de um quadrado:")
 
 lado = int(quadrado_lado)
